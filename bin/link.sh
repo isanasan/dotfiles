cac56060df3ee88ec6aafd466b22916266fa062d
isanasan <yfcgpsebp@gmail.com>
2021-08-27 15:36:47 +0900

    rename dir

--- bin/link.sh
+++ bin/link.sh
@@ -2,8 +2,8 @@
 
 dotfiles_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); cd ../; pwd)
 ln -s ${dotfiles_dir}/config/vimfiles            ~/.vim
-ln -s ${dotfiles_dir}/config/.eskk               ~/.eskk
-ln -s ${dotfiles_dir}/config/.config             ~/.config
-ln -s ${dotfiles_dir}/config/.ctrlp-launcher-lsp ~/.ctrlp-launcher-lsp
+ln -s ${dotfiles_dir}/config/eskk               ~/.eskk
+ln -s ${dotfiles_dir}/config/config             ~/.config
 ln -s ${dotfiles_dir}/config/.skk-jisyo          ~/.skk-jisyo
+ln -s ${dotfiles_dir}/config/p10k.zsh          ~/.p10k.zsh
 
