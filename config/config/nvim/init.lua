304dc01410657b239e6fa24680891b76ca03343e
isanasan <yfcgpsebp@gmail.com>
2021-09-11 13:51:55 +0900

    [change] The source for init.vim is after loading the plugin.

--- config/config/nvim/init.lua
+++ config/config/nvim/init.lua
@@ -1,7 +1,7 @@
-vim.cmd('source ~/.config/nvim/rc/init.vim')
-
 -- Plugins
 require 'rc/pluginlist'
 require 'rc/keymaps'
+
+vim.cmd('source ~/.config/nvim/rc/init.vim')
 
 vim.cmd[[autocmd BufWritePost pluginlist.lua PackerCompile]]
