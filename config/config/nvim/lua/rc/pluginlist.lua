9c5eadc880d35ebcd656de36b4e0dfdef0510f80
isanasan <yfcgpsebp@gmail.com>
2021-12-19 06:30:14 +0900

    Revert "add: ddc-treesitter"
    This reverts commit 39908822c771a53d44ef95b03d8bcd7c31bcdeeb.

--- config/config/nvim/lua/rc/pluginlist.lua
+++ config/config/nvim/lua/rc/pluginlist.lua
@@ -126,7 +126,6 @@ require'packer'.startup{
         use 'Shougo/ddc-sorter_rank'
         use 'shun/ddc-vim-lsp'
         use 'Shougo/ddc-nextword'
-        use 'delphinus/ddc-treesitter'
         use {
             'matsui54/denops-popup-preview.vim',
             config = function() vim.cmd('call popup_preview#enable()') end
