9046467511fb325c14a1e0da835354ae8bdd67c1
isanasan <yfcgpsebp@gmail.com>
2021-12-25 23:03:14 +0900

    update: link cmd

--- bin/mklink.cmd
+++ bin/mklink.cmd
@@ -1,12 +1,12 @@
-mklink /D %USERPROFILE%\vimfiles %USERPROFILE%\dotfiles\config\vimfiles
+mklink /D %USERPROFILE%\vimfiles %USERPROFILE%\ghq\github.com\isanasan\dotfiles\config\vimfiles
 
-mklink /D %USERPROFILE%\.eskk %USERPROFILE%\dotfiles\config\eskk
+mklink /D %USERPROFILE%\.eskk %USERPROFILE%\ghq\github.com\isanasan\dotfiles\config\eskk
 
 mklink /D %USERPROFILE%\.config %USERPROFILE%\ghq\github.com\isanasan\dotfiles\config\config
 
-mklink %USERPROFILE%\.skk-jisyo %USERPROFILE%\dotfiles\config\.skk-jisyo
+mklink %USERPROFILE%\.skk-jisyo %USERPROFILE%\ghq\github.com\isanasan\dotfiles\config\.skk-jisyo
 
-mklink %USERPROFILE%\AppData\Local\vim-lsp-settings\settings.json %USERPROFILE%\dotfiles\config\lsp\settings.json
+mklink %USERPROFILE%\AppData\Local\vim-lsp-settings\settings.json %USERPROFILE%\ghq\github.com\isanasan\dotfiles\config\lsp\settings.json
 
-mklink %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json %USERPROFILE%\dotfiles\config\config\windows-terminal\settings.json
+mklink %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json %USERPROFILE%\ghq\github.com\isanasan\dotfiles\config\config\windows-terminal\settings.json
 
