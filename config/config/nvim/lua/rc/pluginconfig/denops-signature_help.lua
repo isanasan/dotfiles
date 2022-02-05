16337df130568633cecf818aac764af281c55824
isanasan <yfcgpsebp@gmail.com>
2022-02-05 22:43:29 +0900

    update: signature_help style virtual

--- config/config/nvim/lua/rc/pluginconfig/denops-signature_help.lua
+++ config/config/nvim/lua/rc/pluginconfig/denops-signature_help.lua
@@ -5,6 +5,7 @@ local M = {}
         vim.cmd([[
             call signature_help#enable()
             let g:lsp_signature_help_enabled = 0
+            let g:signature_help_config = {'style': "virtual"}
         ]])
 
     end
