75742354053bf9aadf8e19d46c787c2a83c8d243
isanasan <yfcgpsebp@gmail.com>
2022-11-10 15:55:26 +0900

    delete: zoxide

--- config/Microsoft.PowerShell_profile.ps1
+++ config/Microsoft.PowerShell_profile.ps1
@@ -58,7 +58,3 @@ Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
         }
 }
 
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell | Out-String)
-})
